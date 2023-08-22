df12064effe65c54b1a6b34a174dbc1104f65231
Zachary Meyer <Zachary.Meyer@libertymutual.com>
2023-08-22 09:05:11 -0700

    Add _id field and fetch for journal entries

--- src/JournalEntry.js
+++ src/JournalEntry.js
@@ -1,19 +1,17 @@
 import "./JournalEntry.css";
 import { useState, useEffect } from "react";
 
-
-
-const JournalEntry = ({prop}) => {
-	const { title, description, author, skill, date } = prop
-	return (
-		<article className="JournalEntry">
-			<p>Title: {title}</p>
-			<p>Description: {description}</p>
-			<p>Author: {author}</p>
-			<p>Skill: {skill}</p>
-			<p>Date: {date}</p>
-		</article>
-	);
+const JournalEntry = ({ prop }) => {
+  const { _id, title, description, author, skill, date } = prop;
+  return (
+    <article className="JournalEntry" key={_id}>
+      <p>Title: {title}</p>
+      <p>Description: {description}</p>
+      <p>Author: {author}</p>
+      <p>Skill: {skill}</p>
+      <p>Date: {date}</p>
+    </article>
+  );
 };
 
 export default JournalEntry;
--- src/JournalEntryList.js
+++ src/JournalEntryList.js
@@ -1,37 +1,42 @@
 import { useState, useEffect } from "react";
-import JournalEntry from './JournalEntry'
-
+import JournalEntry from "./JournalEntry";
 
 const entrs = [
-	{
-		title: "Some title",
-		description: "blah balh",
-		author: "Jones, Tiffany",
-		skill: "Beginner",
-		date: "1990-01-01",
-	},
-	{
-		title: "Some title",
-		description: "blah balh",
-		author: "Jones, Tiffany",
-		skill: "Beginner",
-		date: "1990-01-01",
-	}
-]
+  {
+    _id: 1,
+    title: "Some title",
+    description: "blah balh",
+    author: "Jones, Tiffany",
+    skill: "Beginner",
+    date: "1990-01-01",
+  },
+  {
+    _id: 2,
+    title: "Some title",
+    description: "blah balh",
+    author: "Jones, Tiffany",
+    skill: "Beginner",
+    date: "1990-01-01",
+  },
+];
 
 const JournalEntryList = () => {
-	const [entries, setEntries] = useState([]);
-	useEffect(() => {
-		setEntries(entrs);
-	}, []);
+  const [entries, setEntries] = useState([]);
+  useEffect(() => {
+    fetch(entrs).then((vars) => setEntries(entrs));
+  }, []);
 
-	return (
-		<section>
-			<ul>
-				{	entries.map(entry => <li><JournalEntry prop={entry} /></li>)	}
-			</ul>
-		</section>
-	)
-}
+  return (
+    <section>
+      <ul>
+        {entries.map((entry) => (
+          <li key={entry._id}>
+            <JournalEntry prop={entry} />
+          </li>
+        ))}
+      </ul>
+    </section>
+  );
+};
 
-export default JournalEntryList
+export default JournalEntryList;
