d64269c0d8b8a0e6b955c9e4ceb2158082d4b708
Jeremy-Goodway_lmig <jeremy.goodway@libertymutual.com>
2023-08-22 10:38:58 -0700

    moved getJournalEntries > BE

--- src/JournalEntryList.js
+++ src/JournalEntryList.js
@@ -1,25 +1,6 @@
 import { useState, useEffect } from "react";
 import JournalEntry from "./JournalEntry";
 
-const entrs = [
-  {
-    _id: 1,
-    title: "Some title",
-    description: "blah balh",
-    author: "Jones, Tiffany",
-    skill: "Beginner",
-    date: "1990-01-01",
-  },
-  {
-    _id: 2,
-    title: "Some title",
-    description: "blah balh",
-    author: "Jones, Tiffany",
-    skill: "Beginner",
-    date: "1990-01-01",
-  },
-];
-
 const JournalEntryList = () => {
   const [entries, setEntries] = useState([]);
   useEffect(() => {
