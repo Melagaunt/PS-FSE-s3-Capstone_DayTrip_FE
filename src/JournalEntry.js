fbc88917e461b35732985fc48ac3c0dea2ab1817
Jones <tiffany.jones@libertymutual.com>
2023-08-24 12:28:05 -0500

    Date

--- src/JournalEntry.js
+++ src/JournalEntry.js
@@ -1,15 +1,37 @@
 import "./JournalEntry.css";
 import { useState, useEffect } from "react";
+const parseDate=(dateTime)=>{
+  const dateObj={date:"",day:"",month:"",year:"", weekday:""};
+  const parsedDate=new Date(Date.parse(dateTime.split("T")[0].replaceAll("-","/")));
+
+  dateObj.date=parsedDate;
+  dateObj.year=parsedDate.getFullYear();
+  dateObj.month=parsedDate.toLocaleString("default",{month:"long"});
+  dateObj.weekday=parsedDate.toLocaleString("default",{weekday:"long"});
+  dateObj.day=parsedDate.getDate();
+  return dateObj
+}
+
 
 const JournalEntry = ({ prop }) => {
   const { _id, title, description, author, skill, date } = prop;
+  const dateObj=parseDate(date)
+  const dateStr=`${dateObj.month} ${dateObj.day}, ${dateObj.year}`
   return (
     <article className="JournalEntry" key={_id}>
-      <p>Title: {title}</p>
-      <p>Description: {description}</p>
-      <p>Author: {author}</p>
-      <p>Skill: {skill}</p>
-      <p>Date: {date}</p>
+      <div>
+        
+        
+        
+        <div><h2>{title}</h2><h3>{author}</h3><h4>{dateStr}</h4>
+        
+        
+        </div>
+        <p>{description}</p>
+        
+        
+        
+        </div>
     </article>
   );
 };
