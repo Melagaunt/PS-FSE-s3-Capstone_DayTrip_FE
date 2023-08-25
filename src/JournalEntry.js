efa66a435df4ef9d349aa07ad0940defdce17c9c
Zachary Meyer <Zachary.Meyer@libertymutual.com>
2023-08-25 07:28:06 -0700

    Initial styling

--- src/JournalEntry.js
+++ src/JournalEntry.js
@@ -1,37 +1,33 @@
 import "./JournalEntry.css";
 import { useState, useEffect } from "react";
-const parseDate=(dateTime)=>{
-  const dateObj={date:"",day:"",month:"",year:"", weekday:""};
-  const parsedDate=new Date(Date.parse(dateTime.split("T")[0].replaceAll("-","/")));
+const parseDate = (dateTime) => {
+  const dateObj = { date: "", day: "", month: "", year: "", weekday: "" };
+  const parsedDate = new Date(
+    Date.parse(dateTime.split("T")[0].replaceAll("-", "/"))
+  );
 
-  dateObj.date=parsedDate;
-  dateObj.year=parsedDate.getFullYear();
-  dateObj.month=parsedDate.toLocaleString("default",{month:"long"});
-  dateObj.weekday=parsedDate.toLocaleString("default",{weekday:"long"});
-  dateObj.day=parsedDate.getDate();
-  return dateObj
-}
-
+  dateObj.date = parsedDate;
+  dateObj.year = parsedDate.getFullYear();
+  dateObj.month = parsedDate.toLocaleString("default", { month: "long" });
+  dateObj.weekday = parsedDate.toLocaleString("default", { weekday: "long" });
+  dateObj.day = parsedDate.getDate();
+  return dateObj;
+};
 
 const JournalEntry = ({ prop }) => {
   const { _id, title, description, author, skill, date } = prop;
-  const dateObj=parseDate(date)
-  const dateStr=`${dateObj.month} ${dateObj.day}, ${dateObj.year}`
+  const dateObj = parseDate(date);
+  const dateStr = `${dateObj.month} ${dateObj.day}, ${dateObj.year}`;
   return (
     <article className="JournalEntry" key={_id}>
       <div>
-        
-        
-        
-        <div><h2>{title}</h2><h3>{author}</h3><h4>{dateStr}</h4>
-        
-        
+        <div>
+          <h2>{title}</h2>
+          <h3>{author}</h3>
+          <h4>{dateStr}</h4>
         </div>
         <p>{description}</p>
-        
-        
-        
-        </div>
+      </div>
     </article>
   );
 };
