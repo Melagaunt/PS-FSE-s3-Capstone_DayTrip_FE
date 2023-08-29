7e9b0051d0bc1351c71454708bbd50af1fd006e7
Zachary Meyer <Zachary.Meyer@libertymutual.com>
2023-08-29 10:09:07 -0700

    Change card arrangement style

--- src/DiaryPage.js
+++ src/DiaryPage.js
@@ -1,21 +1,13 @@
 import "./DiaryPage.css";
 import JournalEntryList from "./JournalEntryList";
 
-const DiaryPage=()=>{
-
-    return(
-<div className="DiaryPage">
-				{/* <div className="page-title">  </div> */}
-				<div className="diary-container">
-					<h1>Diary Page</h1>
-					<JournalEntryList />
-				</div>
-				
-				
-</div>
-
-    )
-
-}
+const DiaryPage = () => {
+  return (
+    <div className="DiaryPage">
+      <h1>Diary Page</h1>
+      <JournalEntryList />
+    </div>
+  );
+};
 
 export default DiaryPage;
