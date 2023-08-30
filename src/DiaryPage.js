e6d00d1f5b6a5e85292da276108b887bcd49d8bc
Jeremy-Goodway_lmig <jeremy.goodway@libertymutual.com>
2023-08-30 12:35:41 -0700

    wip - extract form data

--- src/DiaryPage.js
+++ src/DiaryPage.js
@@ -4,25 +4,53 @@ import JournalEntryList from "./JournalEntryList";
 import { useState } from "react";
 
 const DiaryPage = () => {
-  const [isOpen, setIsOpen] = useState(false);
+	const [isOpen, setIsOpen] = useState(false);
 
-  const changeOpen = () => {
-    setIsOpen(!isOpen);
-  };
+	const changeOpen = () => {
+		setIsOpen(!isOpen);
+	};
 
-  return (
-    <div className="DiaryPage">
-      {isOpen ? <div className="screen"> &nbsp; </div> : null}
-      <div className="formModal">
-        {isOpen ? <FormPage visabilityHandler={changeOpen} /> : null}
-      </div>
-      <div className="content-container">
-      <h1> Diary Page </h1>
-      <button className="open-form-button" onClick={changeOpen}>+ Add New Post</button>
-      <JournalEntryList />
-    </div>
-    </div>
-  );
+	const blankForm = {
+		title: '',
+		description: '',
+		author: '',
+		skill: '',
+		date: '',
+		image: {
+			uri: '',
+			"alt-text": ''
+		},
+	}
+
+	const [formData, setFormData] = useState(blankForm);
+	const changeFormData = (event) => {
+		const {name, value} = event.target;
+		setFormData ( {...formData, [name]:value } )
+	}
+
+	const submitFormHandler = (event) => {
+		event.preventDefault()
+		console.log(formData)
+	}
+
+	return (
+		<div className="DiaryPage">
+			{isOpen ? <div className="screen"> &nbsp; </div> : null}
+			<div className="formModal">
+				{isOpen ? <
+					FormPage visabilityHandler={changeOpen} 
+					formData = {formData} 
+					changeHandler = {changeFormData} 
+					submitHandler = {submitFormHandler}
+				/> : null}
+			</div>
+			<div className="content-container">
+				<h1> Diary Page </h1>
+				<button className="open-form-button" onClick={changeOpen}>+ Add New Post</button>
+				<JournalEntryList />
+			</div>
+		</div>
+	);
 };
 
 export default DiaryPage;
--- src/FormPage.js
+++ src/FormPage.js
@@ -1,22 +1,22 @@
 import "./FormPage.css";
 import { GrClose } from "react-icons/gr";
 
-const FormPage = ({ visabilityHandler }) => {
+const FormPage = ({ visabilityHandler, formData, changeHandler, submitHandler }) => {
   return (
-    <form className="FormPage">
+    <form className="FormPage" onSubmit={submitHandler}>
       <button onClick={visabilityHandler} className="close-button">
         <GrClose />
       </button>
 
       <label for="title">Title</label>
-      <input type="text" id="title" name="title"></input>
+      <input type="text" id="title" name="title" value={formData.title} onChange={changeHandler}></input>
 
       <div className="full-name">
         <label for="fname">First Name</label>
-        <input type="text" id="fname" name="fname"></input>
+        <input type="text" id="fname" name="fname" value={formData.fname} onChange={changeHandler}></input>
 
         <label for="lname">Last Name</label>
-        <input type="text" id="lname" name="lname"></input>
+        <input type="text" id="lname" name="lname" value={formData.lname} onChange={changeHandler}></input>
       </div>
 
       <label for="description">Journal Entry</label>
@@ -25,20 +25,22 @@ const FormPage = ({ visabilityHandler }) => {
         id="description"
         name="description"
         rows="5"
-        cols="30"
+				cols="30"
+				value={formData.description} 
+				onChange={changeHandler}
       ></textarea>
 
       <label for="skill">Skill</label>
-      <select name="skill" id="skill">
-        <option value="beginner">Beginner</option>
-        <option value="beginner">Intermediate</option>
-        <option value="beginner">Expert</option>
+      <select name="skill" id="skill" value={formData.skill} onChange={changeHandler}>
+        <option value="Beginner">Beginner</option>
+        <option value="Intermediate">Intermediate</option>
+        <option value="Expert">Expert</option>
       </select>
 
       <label for="uri">URL</label>
-      <input type="url" id="uri" name="uri"></input>
+      <input type="url" id="uri" name="uri" value={formData.uri} onChange={changeHandler}></input>
 
-      <input type="submit" className="submit-button"></input>
+      <input type="submit" className="submit-button" ></input>
     </form>
   );
 };
