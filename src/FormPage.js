ac2d6f986dd4c1ff869f30a76b57c1b17434f9dd
Jeremy-Goodway_lmig <jeremy.goodway@libertymutual.com>
2023-08-31 12:46:58 -0700

    form pretty-ification

--- src/FormPage.js
+++ src/FormPage.js
@@ -2,47 +2,58 @@ import "./FormPage.css";
 import { GrClose } from "react-icons/gr";
 
 const FormPage = ({ visabilityHandler, formData, changeHandler, submitHandler }) => {
-  return (
-    <form className="FormPage" onSubmit={submitHandler}>
-      <button onClick={visabilityHandler} className="close-button">
-        <GrClose />
-      </button>
+	return (
+		<form className="FormPage" onSubmit={submitHandler}>
+			<button onClick={visabilityHandler} className="close-button">
+				<GrClose  />
+			</button>
+			<div className="form-group">
+				<label for="title">Title</label>
+				<input type="text" id="title" name="title" value={formData.title} onChange={changeHandler}></input>
+			</div>
 
-      <label for="title">Title</label>
-      <input type="text" id="title" name="title" value={formData.title} onChange={changeHandler}></input>
+				<div className="full-name">
+					<div className="form-group">
+						<label for="fname">First Name</label>
+						<input type="text" id="fname" name="fname" value={formData.fname} onChange={changeHandler}></input>
+					</div>
 
-      <div className="full-name">
-        <label for="fname">First Name</label>
-        <input type="text" id="fname" name="fname" value={formData.fname} onChange={changeHandler}></input>
+					<div className="form-group">
+						<label for="lname">Last Name</label>
+						<input type="text" id="lname" name="lname" value={formData.lname} onChange={changeHandler}></input>
+					</div>
+				</div>
 
-        <label for="lname">Last Name</label>
-        <input type="text" id="lname" name="lname" value={formData.lname} onChange={changeHandler}></input>
-      </div>
+			<div className="form-group">
+				<label for="description">Journal Entry</label>
+				<textarea
+					type="textarea"
+					id="description"
+					name="description"
+					rows="5"
+					cols="30"
+					value={formData.description}
+					onChange={changeHandler}
+				></textarea>
+			</div>
 
-      <label for="description">Journal Entry</label>
-      <textarea
-        type="textarea"
-        id="description"
-        name="description"
-        rows="5"
-				cols="30"
-				value={formData.description} 
-				onChange={changeHandler}
-      ></textarea>
+			<div className="form-group">
+				<label for="skill">Skill</label>
+				<select name="skill" id="skill" value={formData.skill} onChange={changeHandler}>
+					<option value="Beginner">Beginner</option>
+					<option value="Intermediate">Intermediate</option>
+					<option value="Expert">Expert</option>
+				</select>
+			</div>
 
-      <label for="skill">Skill</label>
-      <select name="skill" id="skill" value={formData.skill} onChange={changeHandler}>
-        <option value="Beginner">Beginner</option>
-        <option value="Intermediate">Intermediate</option>
-        <option value="Expert">Expert</option>
-      </select>
+			<div className="form-group">
+				<label for="uri">URL</label>
+				<input type="url" id="uri" name="uri" value={formData.uri} onChange={changeHandler}></input>
+			</div>
 
-      <label for="uri">URL</label>
-      <input type="url" id="uri" name="uri" value={formData.uri} onChange={changeHandler}></input>
-
-      <input type="submit" className="submit-button" ></input>
-    </form>
-  );
+			<input type="submit" className="submit-button" ></input>
+		</form>
+	);
 };
 
 export default FormPage;
